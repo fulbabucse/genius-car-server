f223b88ffb32f4b76e02d4af44b5dc68bcb578bb
Fulbabu <fulbabucse@gmail.com>
2022-11-21 02:56:16 +0600

    remove admin

--- index.js
+++ index.js
@@ -95,6 +95,27 @@ const run = async () => {
       res.send(updated);
     });
 
+    app.put("/users/removeAdmin/:id", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await Users.findOne(query);
+
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedInfo = {
+        $unset: {
+          role: "admin",
+        },
+      };
+      const updated = await Users.updateOne(filter, updatedInfo, options);
+      res.send(updated);
+    });
+
     /*
     ------------------------
         Service Database
